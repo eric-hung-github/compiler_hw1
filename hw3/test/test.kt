78a3542cca9bc20f61825546d7451bae2a8ca504
eric-hung-github <50710085+eric-hung-github@users.noreply.github.com>
2022-06-14 03:24:50 +0800

    final

--- hw3/test/test.kt
+++ hw3/test/test.kt
@@ -5,7 +5,9 @@ class Test {
 
     fun checkPrimeNumber(num: int): int {
 
-        for (i in 2..(num/2) ) {
+        var i = 2
+
+        while (i <= (num / 2) ) {
 
             if (num % i == 0) {
                 return 0
