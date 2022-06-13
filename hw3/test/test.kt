5bb4a3917b4aaad7309feb02659a823982cb62dd
eric-hung-github <50710085+eric-hung-github@users.noreply.github.com>
2022-06-13 23:23:43 +0800

    while

--- hw3/test/test.kt
+++ hw3/test/test.kt
@@ -6,7 +6,10 @@ class Test {
         var i = 2
 
         while (i <= num ) {
-
+            if (num % i == 0) {
+                return 0
+            }
+            i = i+1
         }
 
         return 1
