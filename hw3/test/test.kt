824ee0aee5836619aacf02edb8e5876815c2647d
eric-hung-github <50710085+eric-hung-github@users.noreply.github.com>
2022-06-13 21:31:32 +0800

    test

--- hw3/test/test.kt
+++ hw3/test/test.kt
@@ -1,18 +1,12 @@
 class Test {
 
-    fun checkPrimeNumber(num: int): int {
-
-        return 0
-    }
 
     fun main() {
         var low = 20
         val high = 50
 
         while (low < high) {
-            var result = checkPrimeNumber(low) 
-            if (result==1)
-                println(low)
+
 
             low = low + 1
         }
