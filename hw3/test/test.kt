f5aed5753c94de8af3ab145f0ea84328a608f9d0
eric-hung-github <50710085+eric-hung-github@users.noreply.github.com>
2022-06-13 21:33:52 +0800

    t

--- hw3/test/test.kt
+++ hw3/test/test.kt
@@ -10,7 +10,9 @@ class Test {
         val high = 50
 
         while (low < high) {
-
+            var result = checkPrimeNumber(low) 
+            if (result==1)
+                println(low)
 
             low = low + 1
         }
